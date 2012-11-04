94c6e65a7b0ffabf15ff265df5e78b783a32aa23
catseye <cpressey@gmail.com>
2012-11-04 21:56:57 +0000

    Initial import of MMD0 format doc and rudimentary reader script.

---
+++ README.markdown
@@ -0,0 +1,9 @@
+MMD0-tools
+==========
+
+This is a set of Python scripts and modules for working with files in MMD0
+(Amiga MED module) format.
+
+Right now they are in a very crude state.  The ultimate goal is to have
+something that converts most of the data in them to cSound format, where
+they can be cleaned up (consistent panning per instrument, and so forth.)
---
+++ src/mmd0.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+
+class Buffer(object):
+    def __init__(self, file):
+        self.contents = file.read()
+    
+    def ubyte_at(self, pos):
+        return ord(self.contents[pos])
+
+    def ubytes_at(self, pos, count):
+        ubytes = []
+        i = 0
+        while i < count:
+            ubytes.append(self.ubyte_at(pos + i))
+            i += 1
+        return ubytes
+
+    def byte_at(self, pos):
+        u = ord(self.contents[pos])
+        if u >= (256 / 2):
+            u = -1 * (256 - u)
+        return u
+
+    def uword_at(self, pos):
+        return ord(self.contents[pos]) * 256 + ord(self.contents[pos + 1])
+
+    def word_at(self, pos):
+        u = self.uword_at(pos)
+        if u >= (65536 / 2):
+            u = -1 * (65536 - u)
+        return u
+
+    def ulong_at(self, pos):
+        return (ord(self.contents[pos]) * 256 * 256 * 256 +
+                ord(self.contents[pos + 1]) * 256 * 256 +
+                ord(self.contents[pos + 2]) * 256 +
+                ord(self.contents[pos + 3]))
+
+    def offset_at(self, pos):
+        return self.ulong_at(pos)
+
+
+class MMD0(object):
+    def __init__(self, buffer):
+        self.buffer = buffer
+        self.id = buffer.ulong_at(0)
+        self.modlen = buffer.ulong_at(4)
+        self.MMD0song_offset = buffer.offset_at(8)
+        # offset to table of offsets
+        self.MMD0Block_offset = buffer.offset_at(16)
+        # offset to table of offsets
+        self.InstrHdr_offset = buffer.offset_at(24)
+        self.MMD0exp_offset = buffer.offset_at(32)
+        self.pstate = buffer.uword_at(40)
+        self.pblock = buffer.uword_at(42)
+        self.pline = buffer.uword_at(44)
+        self.pseqnum = buffer.uword_at(46)
+        self.actplayline = buffer.word_at(48)
+        self.counter = buffer.ubyte_at(50)
+        self.extra_songs = buffer.ubyte_at(51)
+
+        # load the MMD0song structure
+        self.MMD0song = MMD0song(buffer, self.MMD0song_offset)
+        # XXX load the block offset table, now that we know numblocks
+
+    def dump(self):
+        for attr in ('id', 'modlen', 'MMD0song_offset', 'MMD0Block_offset',
+                     'InstrHdr_offset', 'MMD0exp_offset',
+                     'pstate', 'pblock', 'pline', 'pseqnum',
+                     'actplayline', 'counter', 'extra_songs'):
+            print "%s: %r" % (attr, getattr(self, attr))
+        print "---"
+        self.MMD0song.dump()
+
+
+class MMD0song(object):
+    def __init__(self, buffer, offset):
+        self.buffer = buffer
+        
+        sample_offset = offset
+        self.sample = []
+        i = 0
+        while i < 63:
+            #self.sample.append(MMD0sample(buffer, sample_offset)
+            sample_offset += 8
+            i += 1
+
+        self.numblocks = buffer.uword_at(offset + 504)
+        self.songlen = buffer.uword_at(offset + 506)
+        self.playseq = buffer.ubytes_at(offset + 508, 256)
+        self.deftempo = buffer.uword_at(offset + 764)
+        self.playtransp = buffer.byte_at(offset + 766)
+        self.flags = buffer.ubyte_at(offset + 767)
+        self.flags2 = buffer.ubyte_at(offset + 768)
+        self.tempo2 = buffer.ubyte_at(offset + 769)
+        self.trkvol = buffer.ubytes_at(offset + 770, 16)
+        self.mastervol = buffer.ubyte_at(offset + 786)
+        self.numsamples = buffer.ubyte_at(offset + 787)
+
+    def dump(self):
+        # XXX dump sample structures
+        for attr in ('numblocks', 'songlen', 'playseq', 'deftempo',
+                     'playtransp', 'flags', 'flags2', 'tempo2',
+                     'trkvol', 'mastervol', 'numsamples'):
+            print "%s: %r" % (attr, getattr(self, attr))
+
+
+if __name__ == '__main__':
+    import sys
+    b = None
+    with open(sys.argv[1], 'r') as f:
+        b = Buffer(f)
+    m = MMD0(b)
+    m.dump()
